831adc7ab6a8df1e6959b5f991aa8a9b5dc934e7
Bagno <bagnodimitrti@yandex.ru>
2026-03-04 09:22:12 +0500

    Initial commit

---
+++ resources/js/custom.js
@@ -0,0 +1,101 @@
+/*
+	Future Imperfect by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+*/
+// Source - https://stackoverflow.com/a/49313025
+// Posted by Rafael A. Cespedes Basteiro, modified by community. See post 'Timeline' for change history
+// Retrieved 2026-02-27, License - CC BY-SA 4.0
+
+import jQuery from "jquery";
+const $ = jQuery;
+window.$ = $;
+
+(function($) {
+
+
+
+	$(function() {
+
+		var	$window = $(window),
+			$body = $('body'),
+			$menu = $('#menu'),
+			$sidebar = $('#sidebar'),
+			$main = $('#main');
+
+		// Disable animations/transitions until the page has loaded.
+			$body.addClass('is-loading');
+
+			$window.on('load', function() {
+				window.setTimeout(function() {
+					$body.removeClass('is-loading');
+				}, 100);
+			});
+
+		// Fix: Placeholder polyfill.
+			$('form').placeholder();
+
+		// Prioritize "important" elements on medium.
+
+		// IE<=9: Reverse order of main and sidebar.
+
+		// Menu.
+			$menu
+				.appendTo($body)
+				.panel({
+					delay: 500,
+					hideOnClick: true,
+					hideOnSwipe: true,
+					resetScroll: true,
+					resetForms: true,
+					side: 'right',
+					target: $body,
+					visibleClass: 'is-menu-visible'
+				});
+
+		// Search (header).
+			var $search = $('#search'),
+				$search_input = $search.find('input');
+
+			$body
+				.on('click', '[href="#search"]', function(event) {
+
+					event.preventDefault();
+
+					// Not visible?
+						if (!$search.hasClass('visible')) {
+
+							// Reset form.
+								$search[0].reset();
+
+							// Show.
+								$search.addClass('visible');
+
+							// Focus input.
+								$search_input.focus();
+
+						}
+
+				});
+
+			$search_input
+				.on('keydown', function(event) {
+
+					if (event.keyCode == 27)
+						$search_input.blur();
+
+				})
+				.on('blur', function() {
+					window.setTimeout(function() {
+						$search.removeClass('visible');
+					}, 100);
+				});
+
+		// Intro.
+			var $intro = $('#intro');
+
+			// Move to main on <=large, back to sidebar on >large.
+
+	});
+
+})(jQuery);
---
+++ resources/js/main.js
@@ -0,0 +1,7 @@
+import { createApp } from 'vue';
+import App from './App.vue';
+import '../css/main.css';
+import 'font-awesome/css/font-awesome.min.css'
+import "./custom.js"
+import "./util.js"
+createApp(App).mount('#app');
